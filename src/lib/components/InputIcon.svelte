84d7884889ef2aa18414e554802b672f00730b8f
ryan pujo <pujotriantoro@gmail.com>
2023-12-11 01:02:26 +0700

    now <InputIcon> component has the ability to add prepend or append icon complete with its on:click handler

--- src/lib/components/InputIcon.svelte
+++ src/lib/components/InputIcon.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
-
+import Icon from '@iconify/svelte';
+	import type { MouseEventHandler } from 'svelte/elements';
 
 export let type = 'text';
+export let prependIcon = '';
+export let appendIcon = '';
+export let appendIconOnClick: MouseEventHandler<HTMLButtonElement> | null = null;
+export let prependIconOnClick: MouseEventHandler<HTMLButtonElement> | null = null;
 </script>
 
 <div class="relative flex-grow flex h-fit items-center rounded-lg bg-gray-100 dark:bg-gray-700 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600">
-  <input {type} class="h-10 bg-gray-100 dark:bg-gray-700 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600 rounded-tl-lg rounded-bl-lg focus:ring-0" placeholder="Search">
-  <button class="btn btn-lg variant-filled-primary rounded-none rounded-tr-lg rounded-br-lg">
-    <slot name="prependIcon" />
+  {#if prependIcon}
+  <button class="mx-2" disabled={prependIconOnClick == null} on:click|preventDefault={prependIconOnClick}>
+    <Icon class='' height='2em' icon={prependIcon} />
   </button>
+  {/if}
+  
+  <input {type} class="h-10 bg-gray-100 dark:bg-gray-700 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600 rounded-tl-lg rounded-bl-lg focus:ring-0 border-transparent focus:border-transparent" placeholder="Search">
+  
+  {#if appendIcon}
+  <button class="mx-2" disabled={appendIconOnClick == null} on:click|preventDefault={appendIconOnClick}>
+    <Icon height='2em' icon={appendIcon} />
+  </button>
+  {/if}
 </div>
