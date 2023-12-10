d25f4aec5efaaf07bab6b49bf3d58a2699394261
ryan pujo <pujotriantoro@gmail.com>
2023-12-11 01:05:21 +0700

    reflecting to the last changes on <InputIcon> component

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	import authStore from "$lib/stores/authStore";
-	import { AppBar, getDrawerStore, type DrawerSettings } from "@skeletonlabs/skeleton";
+	import { AppBar, getDrawerStore, type DrawerSettings, LightSwitch } from "@skeletonlabs/skeleton";
 	import List from "$lib/components/list/List.svelte";
 	import InputIcon from "$lib/components/InputIcon.svelte";
 	import ListItem from "$lib/components/list/ListItem.svelte";
-	import ListItemTitle from "$lib/components/list/ListItemTitle.svelte";
-	import ListItemSubtitle from "$lib/components/list/ListItemSubtitle.svelte";
-	// @ts-ignore
 	import Icon from '@iconify/svelte';
 
 	const drawerSettings: DrawerSettings = {
@@ -21,8 +18,8 @@
 const title = $authStore.isLoggedIn ? $authStore.user?.displayName : 'Sign in';
 const subtitle = $authStore.isLoggedIn ? $authStore.user?.email : 'Account';
 const imageUrl = 'https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop';
-	const drawerStore = getDrawerStore();
-	const openDrawer = () => drawerStore.open(drawerSettings);
+const drawerStore = getDrawerStore();
+const openDrawer = () => drawerStore.open(drawerSettings);
 </script>
 
 <AppBar slotLead="" slotDefault="place-self-center">
@@ -32,13 +29,8 @@ const imageUrl = 'https://images.unsplash.com/photo-1617296538902-887900d9b592?i
 			<Icon icon='mdi:menu' color='#ffffff' />
 		</button>
   </svelte:fragment>
-  <svelte:fragment>
-    <InputIcon>
-			<svelte:fragment slot="prependIcon">
-				<Icon icon='mdi:magnify' />
-			</svelte:fragment>
-		</InputIcon>
-  </svelte:fragment>
+    <InputIcon appendIcon='mdi:magnify' />
+		
   <svelte:fragment slot="trail">
 		<List class='flex'>
 			<ListItem 
@@ -46,25 +38,22 @@ const imageUrl = 'https://images.unsplash.com/photo-1617296538902-887900d9b592?i
 				iconSize='2em'
 				title='Wishlist'
 				subtitle='My Items'
-			>
-			</ListItem>
+			/>
 			<ListItem 
 				icon='mdi:cart-plus' 
 				iconSize='2em'
 				title='Total'
 				subtitle='Price'
-			>
-			</ListItem>
+			/>
 			<ListItem 
 				prependAvatar={$authStore.isLoggedIn ? imageUrl : undefined} 
 				icon={$authStore.isLoggedIn ? undefined : 'mdi:account'}
 				iconSize='2em'
 				title={title}
 				subtitle={subtitle}
-			>
-			</ListItem>
+			/>
 		</List>
-    <!-- <LightSwitch /> -->
+    <LightSwitch />
 
     <!-- <Avatar src="/nika.png" width="w-9" cursor="cursor-pointer" />
     <DropdownMenu /> -->
@@ -73,6 +62,3 @@ const imageUrl = 'https://images.unsplash.com/photo-1617296538902-887900d9b592?i
 <div class="w-full h-fit hidden lg:flex justify-between px-4 py-2 text-sm list-nav">
 	<slot name="extension"/>
 </div>
-<style>
-
-</style>
